f297b3a72ebb5635567353199e6ae6f1d2e450fc
FelipePilz <felipevilvertpilz@gmail.com>
2021-11-07 22:00:10 -0300

    Criando primeiro service

---
+++ src/app/services/transferencia.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransferenciaService {
+  private listaTransferencia: any[];
+
+  constructor() {
+    this.listaTransferencia = [];
+    // Inicializando o atributo
+  }
+
+  get transferencias() {
+    return this.listaTransferencia;
+    //Getter do atributo
+  }
+
+  adicionar(transferencia: any) {
+    this.hidratar(transferencia);
+    this.listaTransferencia.push(transferencia);
+  }
+
+  private hidratar(transferencia: any) {
+    transferencia.data = new Date();
+  }
+}
